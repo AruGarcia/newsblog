22baa44ae4b1751cc07bd916df567bd7cb716d11
Aruanã (ACosta) Garcia <120034499+AruGarcia@users.noreply.github.com>
2023-11-28 11:16:59 -0300

    Add view newspaper home, staticfiles and add the template to the project. (#8)

--- newspaper/base/views.py
+++ newspaper/base/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page_view(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'base/home.html')
