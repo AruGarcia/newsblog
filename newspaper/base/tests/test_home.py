65873c91b69c322e67b56965d3b219a25dfd7e68
Aruanã Garcia <aruanagarcia@gmail.com>
2023-11-29 00:19:43 -0300

    Edit home.html and add tests for title e home_link. Named urls add to the project.

--- newspaper/base/tests/test_home.py
+++ newspaper/base/tests/test_home.py
@@ -1,6 +1,22 @@
-from django.test import Client
+import pytest
+from django.urls import reverse
+
+from newspaper.django_assetions import assert_contains
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>NewsBlog</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}" class="logo m-0 float-start">NewsBlog')
