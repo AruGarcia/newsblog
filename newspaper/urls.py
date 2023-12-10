18e95b4f0e3f8a4afa5474c2d2f3f42e85b4b3e3
Aruanã Garcia <aruanagarcia@gmail.com>
2023-12-10 13:55:53 -0300

    mod in blog.html

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('newspaper.base.urls')),
-    path('', include('newspaper.inst.urls')),
-    path('', include('newspaper.blog.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('newspaper.base.urls')),
+                  path('', include('newspaper.inst.urls')),
+                  path('', include('newspaper.blog.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
