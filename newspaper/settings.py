5905fc11e16dc0a85779af65cc99ba6d45005d38
Aruanã Garcia <aruanagarcia@gmail.com>
2024-04-16 17:40:29 -0300

    atualização dia 16 de abril.

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 STATIC_MEDIA = os.path.join(BASE_DIR, 'mediafiles')
